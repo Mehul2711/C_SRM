0e4f2ae43955dee55a93b86356cb8e5640ba0d99
Mehul2711 <mehul271102@gmail.com>
2022-04-22 21:41:09 +0530

    Update

---
+++ pps.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+  int num1,num2,num3,num4,addition;
+  scanf("%d\n%d\n%d\n%d",&num1,&num2,&num3,&num4);
+  addition=num1+num2+num3+num4;
+  printf("%d",addition);
+  return 0;
+}
